83d66e0b184edb30ececec723b6483704d2c6eda
Maria Segura <ext.maria.segura@ar.infra.d>
2020-10-24 18:29:57 -0300

    Javalin, path /hamburguesa

--- src/main/java/maria/clara/Main.java
+++ src/main/java/maria/clara/Main.java
@@ -1,7 +1,14 @@
 package maria.clara;
 
+import io.javalin.Javalin;
+import maria.clara.servicios.ServicioInventario;
+
 public class Main {
     public static void main(String[] args) {
-        
+        ServicioInventario inventario = new ServicioInventario();
+        Javalin app = Javalin.create().start(7000);
+        app.get("/hamburguesa", ctx -> ctx.result(String.valueOf(inventario.obtenerInventario("Hamburguesa"))));
     }
 }
+
+
