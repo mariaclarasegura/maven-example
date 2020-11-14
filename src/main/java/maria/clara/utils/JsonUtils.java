0ef285319b8046631f2be0af8095bf9dce41741e
Maria Segura <ext.maria.segura@ar.infra.d>
2020-11-14 00:02:12 -0300

    Json keys to snake case. First attempt to wrap objects with class name

--- src/main/java/maria/clara/utils/JsonUtils.java
+++ src/main/java/maria/clara/utils/JsonUtils.java
@@ -1,12 +1,16 @@
 package maria.clara.utils;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
+@JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
 public final class JsonUtils {
     private static ObjectMapper jsonMapper = new ObjectMapper();
 
     public static String toJson(Object object) throws JsonProcessingException {
-        return jsonMapper.writeValueAsString(object);
+        return jsonMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE).writeValueAsString(object);
     }
 }
+
