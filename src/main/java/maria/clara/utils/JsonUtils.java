6979788a26f505f8f13983f73c079a7c8e203142
Maria Segura <ext.maria.segura@ar.infra.d>
2021-01-17 20:46:55 -0300

    Json Utils

--- src/main/java/maria/clara/utils/JsonUtils.java
+++ src/main/java/maria/clara/utils/JsonUtils.java
@@ -1,7 +1,6 @@
 package maria.clara.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.type.CollectionType;
